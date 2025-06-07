2469d5d6d1e85cd3b13ec440d8e081861f27942b
Alexey Tukin <tukin2k@gmail.com>
2025-06-07 14:20:33 +0300

    Создал архив с файлами

---
+++ test_create_zip_files.py
@@ -0,0 +1,7 @@
+from zipfile import ZipFile
+
+with ZipFile("tmp/archive_files.zip", 'w') as zip_file: # создаем архив
+    zip_file.write("tmp/promo.xlsx", arcname='promo.xlsx') # добавляем файл в архив
+    zip_file.write("tmp/ДКП.pdf", arcname='ДКП.pdf')  # добавляем файл в архив
+    zip_file.write("tmp/promocodes.csv", arcname='promocodes.csv')  # добавляем файл в архив
+    zip_file.close() # закрываем архив
