059ac1f0b24f0872668748091c4afe5ae30571fc
Alexey Tukin <tukin2k@gmail.com>
2025-06-10 11:17:36 +0300

    Правки после проверки

--- test_create_zip_files.py
+++ test_create_zip_files.py
@@ -26,7 +26,7 @@ def test_xlsx_file(create_zip_file):
     check_value = "4ZN97MR"
     with ZipFile("tmp/archive_files.zip") as zip_file:
         print(zip_file.namelist())
-        xlsx_file = zip_file.open('promo_codes.xlsx')
+        xlsx_file = zip_file.open('promo.xlsx')
         df = pd.read_excel(xlsx_file)
         a = df.apply(lambda x: x.str.contains(check_value, na=False)).any().any()
         assert a == True
